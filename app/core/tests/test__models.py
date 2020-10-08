19d1ec45223049059893df95147566d55b663b2f
Franco Lema <francoe.lema.dev@gmail.com>
2020-10-08 14:31:36 -0500

    Update admin to support custom user model

--- app/core/tests/test__models.py
+++ app/core/tests/test__models.py
@@ -32,6 +32,6 @@ class ModelTests(TestCase):
             'test@mail.com',
             'test123'
         )
-
+        
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
