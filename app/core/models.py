861ce9beb6591cf0568d20ef147be1e570b5ec2f
Franco Lema <francoe.lema.dev@gmail.com>
2020-10-11 11:43:11 -0500

    Added endpoint for creating and authenticating users

--- app/core/models.py
+++ app/core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 class UserManager(BaseUserManager):
-
+    
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new user """
         if not email:
@@ -23,7 +23,7 @@ class UserManager(BaseUserManager):
         return user
 
 class User(AbstractBaseUser, PermissionsMixin):
-    """Custom user model that supports """
+    """Custom user model that supports"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
